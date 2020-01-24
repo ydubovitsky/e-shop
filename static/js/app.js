270f91f6dc86df7568dfc52dcb32c5f62a4ea3fd
y.dubovitsky <y.dubovitsky@gmail.con>
2020-01-24 19:05:45 +0300

    Added an event handler to the 'go' button and checkbox block. It is necessary to fix the removal of goods from the basket (does not work).

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,14 @@
-;$(function() {
+;
+$(function () {
 
-    let init = function() {
+    let init = function () {
         initBuyBtn();
         initAddToCart();
         $('#addProductPopup .count').change(calculateCost);
         $('#loadMore').click(loadMoreProducts);
+        initSearchForm();
+        $('#goSearch').click(goSearch);
+        $('.remove-product').click(removeProductFromCart);
     };
 
     /**
@@ -12,9 +16,9 @@
      * Функция заполняет Popup.
      * Для каждого товара, создается свой popup, c параметрами этого товара
      */
-    let showAddProductPopup = function() {
+    let showAddProductPopup = function () {
         let idProduct = $(this).attr('data-id-product'); // как тут работает this?
-        let product = $('#product'+idProduct);
+        let product = $('#product' + idProduct);
         $('#addProductPopup').attr('data-id-product', idProduct); // атрибуту присваивается значение(idProduct)
         $('#addProductPopup .product-image').attr('src', product.find('.thumbnail img').attr('src'));
         $('#addProductPopup .name').text(product.find('.name').text());
@@ -32,16 +36,19 @@
     /**
      * Присваивает кнопке обработчик по клику.
      */
-    let initBuyBtn = function() {
+    let initBuyBtn = function () {
         $('.buy-btn').click(showAddProductPopup);
     };
 
-    let addProductToCart = function() {
+    /**
+     * Добавляем товары в корзину
+     */
+    let addProductToCart = function () {
         let idProduct = $('#addProductPopup').attr('data-id-product'); // считывает значение атрибута
         let count = $('#addProductPopup .count').attr('value');
         $('#addToCartIndi').removeClass('d-none');
         $('#addToCart').addClass('d-none');
-        setTimeout(function() {
+        setTimeout(function () {
             let data = {
                 totalCount: count,
                 totalCost: 2000
@@ -55,20 +62,20 @@
         }, 800);
     };
 
-    let initAddToCart = function() {
+    let initAddToCart = function () {
         $('#addToCart').click(addProductToCart);
-    }
+    };
 
     /**
-     * 
+     * Вычисляем стоимость в зависимости от количества товара
      */
-    let calculateCost = function() {
+    let calculateCost = function () {
         let priceStr = $('#addProductPopup .price').text();
         let price = parseFloat(priceStr.replace('$', ' '));
         let count = parseInt($('#addProductPopup .count').val());
         let min = parseInt($('#addProductPopup .count').attr('min'));
         let max = parseInt($('#addProductPopup .count').attr('max'));
-        if(count >= min && count <= max) {
+        if (count >= min && count <= max) {
             let cost = price * count;
             $('#addProductPopup .cost').text('$ ' + cost);
         } else {
@@ -81,14 +88,114 @@
     /**
      * Обработчик нажатия кнопки загрузить еще
      */
-    let loadMoreProducts = function() {
+    let loadMoreProducts = function () {
         $('#loadMore').addClass('d-none');
         $('#loadMoreIndicator').removeClass('d-none');
-        setTimeout(function() {
+        setTimeout(function () {
             $('#loadMoreIndicator').addClass('d-none');
             $('#loadMore').removeClass('d-none');
         }, 800);
     };
 
+    /**
+     * Инициализация checkbox
+     */
+    let initSearchForm = function () {
+        // categories
+        $('#allCategories').click(function () {
+            $('.categories .search-option').prop('checked', $(this).is(':checked')); // если выбраны все категории, то ставятся везде галки
+        });
+        $('.categories .search-option').click(function () {
+            $('#allCategories').prop('checked', false);
+        });
+        // allProducers
+        $('#allProducers').click(function () {
+            $('.producers .search-option').prop('checked', $(this).is(':checked')); // если выбраны все категории, то ставятся везде галки
+        });
+        $('.producers .search-option').click(function () {
+            $('#allProducers').prop('checked', false);
+        });
+    };
+
+    /**
+     * Функция поиска
+     */
+    let goSearch = function () {
+        /**
+         * selector - массив элементов.
+         * value - сам объект-селектор из массива. Если у каждого селектора стоит состояние :checked, тогда 
+         * увеличивается счетчик.
+         * @param {} selector 
+         */
+        var isAllSelected = function (selector) {
+            let unchecked = 0;
+            $(selector).each(function (index, value) {
+                if (!($(value).is(':checked'))) {
+                    unchecked++;
+                }
+            });
+            return unchecked === 0;
+        };
+        if (isAllSelected('.categories .search-option')) {
+            // если выбраны все элементы, то галочки снимаются
+            $('.categories .search-option').prop('checked', false);
+        }
+        if (isAllSelected('.producers .search-option')) {
+            $('.producers .search-option').prop('checked', false);
+        }
+        $('form.search').submit();
+    };
+
+    let confirn = function(msg, okFunction) {
+        if(window.confirm(msg)) {
+            okFunction();
+        }
+    };
+
+    let removeProductFromCart = function() {
+        let btn = $(this);
+        confirm("Are you shure?", function() {
+            executeRemoveProduct(btn);
+        });
+    };
+
+    let executeRemoveProduct = function(btn) {
+        let idProduct = btn.attr('data-id-product');
+        let countProduct = btn.attr('data-count');
+        btn.removeClass('btn-danger');
+        btn.removeClass('btn');
+        btn.addClass('load-indicator'); // стилизировали кнопку, но нужно удалить текст и обработчик
+        let text = btn.text();
+        btn.text('');
+        btn.off('click');
+
+        setTimeout(function() {
+            let data = {
+                totalCount: 1,
+                totalCost: 555
+            };
+            if(data.totalCount === 0) {
+                window.location.href = 'products.html';
+            } else {
+                let prevCount = parseInt($('#product' + idProduct + ' .count').text());
+                let remCount = parseInt(count);
+                if(remCount === prevCount) {
+                    $('#product' + idProduct).remove();
+                } else {
+                    btn.removeClass('load-indicator');
+                    btn.addClass('btn');
+                    btn.addClass('btn-danger');
+                    btn.text(text);
+                    btn.click(removeProductFromCart);    
+                    $('#product' + idProduct + ' .count').text(prevCount - remCount);   
+                    if(prevCount - remCount == 1) {
+                        $('#product' + idProduct + ' a.remove-product.all').remove();
+                    }
+                }
+            }
+        }, 1000);
+    };
+
+    // Инициализирует функцию, которая все запускает
     init();
 });
